cb1f8d48ce00acc058116b68fc67454ec320dcf5
Kelvin Chappell <kelvin.chappell@theguardian.com>
2024-01-24 14:26:47 +0000

    Format code

--- app/load/AppComponents.scala
+++ app/load/AppComponents.scala
@@ -5,7 +5,9 @@ import play.api.BuiltInComponentsFromContext
 import play.filters.HttpFiltersComponents
 import router.Routes
 
-class AppComponents(context: Context) extends BuiltInComponentsFromContext(context) with HttpFiltersComponents {
+class AppComponents(context: Context)
+    extends BuiltInComponentsFromContext(context)
+    with HttpFiltersComponents {
   lazy val healthCheckController = new controllers.HealthCheckController(controllerComponents)
   lazy val router: Routes = new Routes(httpErrorHandler, healthCheckController)
 }
