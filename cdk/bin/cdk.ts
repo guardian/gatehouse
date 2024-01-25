8e3e4755e5b1731e04e723ab5807158c83801d1a
Kelvin Chappell <kelvin.chappell@theguardian.com>
2024-01-25 09:28:47 +0000

    Make domain names more conventional
    Now clearer that these domains are internal.

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -14,12 +14,12 @@ new Gatehouse(app, 'gatehouse-CODE', {
     stack,
     env,
     stage: 'CODE',
-    domainName: 'gatehouse.code.dev-guardianapis.com',
+    domainName: 'gatehouse-origin.code.dev-guardianapis.com',
 });
 
 new Gatehouse(app, 'gatehouse-PROD', {
     stack,
     env,
     stage: 'PROD',
-    domainName: 'gatehouse.guardianapis.com',
+    domainName: 'gatehouse-origin.guardianapis.com',
 });
