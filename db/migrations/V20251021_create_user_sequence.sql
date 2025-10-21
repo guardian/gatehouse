aff3a1247b477c390a0a7081c00e87dc7cd5a7a1
Akinsola Lawanson <akinsola.lawanson@guardian.co.uk>
2025-10-21 14:06:24 +0100

    Create User Sequence migration file.

---
+++ db/migrations/V20251021_create_user_sequence.sql
@@ -0,0 +1,6 @@
+-- Migration: V20251021_create_user_sequence.sql
+-- Description: Create a sequence for user IDs and set its starting value
+
+CREATE SEQUENCE IF NOT EXISTS useridseq; 
+SELECT setval('useridseq', 114007520 + 10000); -- Set starting value to the latest user ID in Identity DB -- Add 10000 to avoid conflicts with existing IDs
+GRANT USAGE, SELECT ON SEQUENCE useridseq TO identity_api;
