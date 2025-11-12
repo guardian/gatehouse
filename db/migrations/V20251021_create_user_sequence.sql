6d5a0fd98c2cbc297ed5cffe47b4b695ae4b0b4d
Akinsola Lawanson <akinsola.lawanson@guardian.co.uk>
2025-11-12 12:19:16 +0000

    Use the biggest value in the users table instead of hardcoding a value.

--- db/migrations/V20251021_create_user_sequence.sql
+++ db/migrations/V20251021_create_user_sequence.sql
@@ -2,5 +2,5 @@
 -- Description: Create a sequence for user IDs and set its starting value
 
 CREATE SEQUENCE IF NOT EXISTS useridseq; 
-SELECT setval('useridseq', 114007520 + 10000); -- Set starting value to the latest user ID in Identity DB -- Add 10000 to avoid conflicts with existing IDs
+SELECT setval('useridseq', (SELECT MAX(id::BIGINT) FROM users) + 100000); -- Set starting value to the latest user ID in Identity DB -- Add 100000 to avoid conflicts with existing IDs
 GRANT USAGE, SELECT ON SEQUENCE useridseq TO identity_api;
