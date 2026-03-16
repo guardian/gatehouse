64a5635b0d40a36f3a0f7c2af2d252800cc136bc
AshCorr <ashcorr20@gmail.com>
2026-03-16 09:59:55 +0000

    chore: Remove un-used Gatehouse API app

--- app/utils/RequestHelper.scala
+++
@@ -1,9 +0,0 @@
-package utils
-
-import play.api.http.HeaderNames.X_FORWARDED_FOR
-import play.api.mvc.RequestHeader
-
-object RequestHelper {
-
-  def origin(request: RequestHeader): String = request.headers.get(X_FORWARDED_FOR).getOrElse(request.remoteAddress)
-}
